8c4339e2e5c1705dc45816a52cc7a486d6d0dcac
제리 <jaryapp@gmail.com>
2020-11-09 23:22:08 +0900

    feature: available zones

--- index.js
+++ index.js
@@ -10,14 +10,39 @@ function getListInstance() {
       if (err) {
         console.log(err, err.stack);
         reject(err);
-      }
-      else resolve(data);
+      } else resolve(data);
+    });
+  });
+}
+
+// 1. list instance
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/EC2.html#describeInstances-property
+function getListInstance() {
+  return new Promise((resolve, reject) => {
+    ec2.describeInstances(function (err, data) {
+      if (err) {
+        console.log(err, err.stack);
+        reject(err);
+      } else resolve(data);
+    });
+  });
+}
+
+// 2. available zones
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/EC2.html#describeAvailabilityZones-property
+function getAvailableZones() {
+  return new Promise((resolve, reject) => {
+    ec2.describeAvailabilityZones(function (err, data) {
+      if (err) {
+        console.log(err, err.stack);
+        reject(err);
+      } else resolve(data);
     });
   });
 }
 
 async function main() {
-  let list = await getListInstance();
+  let list = await getAvailableZones();
   console.log(list);
 }
 
